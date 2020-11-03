7d648fa570eaea9afccc201d8f2f2e35ba9b77bf
mrToad1986 <63360585+mrToad1986@users.noreply.github.com>
2020-11-04 01:30:48 +0300

    Add files via upload

---
+++ example.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE example;
+USE example;
+CREATE TABLE users(
+	id int,
+    name varchar(255)
+);
+INSERT INTO users VALUES (1, 'name_1');
+INSERT INTO users VALUES (2, 'name_2');
+INSERT INTO users VALUES (3, 'name_3');
